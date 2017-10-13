26c5d7cfcd50efad2c4d1d3abda08763773337eb
Chobits <bearnewmac@xiongrandeMacBook-Pro.local>
2017-10-13 21:14:59 +0800

    Commit

---
+++ BearApplication/Bear Base App.xctemplate/Constant/Constant.h
@@ -0,0 +1,13 @@
+//
+//  Constant.h
+//  ___PROJECTNAME___
+//
+//  Created by Chobits.
+//  Copyright © 2017年 Chobits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constant : NSObject
+
+@end
---
+++ BearApplication/Bear Base App.xctemplate/Constant/Constant.m
@@ -0,0 +1,13 @@
+//
+//  Constant.m
+//  ___PROJECTNAME___
+//
+//  Created by Chobits.
+//  Copyright © 2017年 Chobits. All rights reserved.
+//
+
+#import "Constant.h"
+
+@implementation Constant
+
+@end
