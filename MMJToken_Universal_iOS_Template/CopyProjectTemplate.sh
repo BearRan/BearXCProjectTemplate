51f6be0a5a22a04d236240af1f275080263e68f9
Chobits <bearnewmac@xiongrandeMacBook-Pro.local>
2017-10-13 21:38:11 +0800

    临时备份一下

---
+++ MMJToken_Universal_iOS_Template/CopyProjectTemplate.sh
@@ -0,0 +1,12 @@
+#currentShellPath
+currentShellPath=$(cd `dirname $0`; pwd)
+echo "\n""currentShellPath:\n"$currentShellPath
+
+#destinationPath
+destinationPath="/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project Templates"
+echo "\n""destinationPath:\n"$destinationPath
+
+#copy
+cp -Rf $currentShellPath /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates
+
+echo "\nCopy Finish!"
